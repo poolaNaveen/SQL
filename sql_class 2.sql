21678e0d25489f2f143cd7210bf5361088b3b27c
Poola Naveen <naveenpoola04@gmail.com>
2025-10-07 23:52:45 +0530

    Add files via upload

---
+++ sql_class 2.sql
@@ -0,0 +1,62 @@
+-- select * from hr.employees
+-- select first_name, upper(first_name) from hr.employees
+-- select first_name,lower(first_name) from hr.employees
+-- select initcap('edukron for data science') as title ,length('edukron') from dual 
+-- select substr('data science', 2,5) from dual
+-- select instr('data science', 'science') from dual
+-- select rpad('123',7,'0') from dual
+-- select lpad('123',5,'0')
+-- select lpad('123',6,'1') from dual
+-- select trim(' data     ') from dual it is used for to trim the spaces.
+-- select replace('data science', 'science','engineering')from dual
+-- select concat('oracle',' ','sql') from dual
+-- select ascii('a') from dual
+-- select reverse('naveen') from dual
+-- select * from hr.employees
+-- desc hr.employees
+-- select email,length(email) from hr.EMPLOYEES
+-- select email,substr(email,1,4) from hr.EMPLOYEES
+-- select phone_number from hr.employees
+-- select first_name,last_name from hr.EMPLOYEES
+-- select substr(first_name,1,3)|| substr(last_name,1,3)from hr.EMPLOYEES
+-- select phone_number,rpad('x',length(phone_number)-4,'x')|| substr(phone_number,-4) as masked_phonenumber from hr.employees
+--FUNCTIONS IN NUMBERS
+-- select ceil(12.9) from dual
+-- select sign(-23), sign(10),sign(0) from dual 
+--select greatest(20,30,89,98,100)from dual
+-- select least(23,9,5,3) from dual
+-- select round(123.56,2) from dual
+
+-- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
