ee9b82fc0dae568b40bc85f329af45cbe30243ac
Poola Naveen <naveenpoola04@gmail.com>
2025-10-08 21:25:07 +0530

    Add files via upload

---
+++ sql_class3.sql
@@ -0,0 +1,14 @@
+-- select employee_id,first_name,last_name,hire_date,sysdate from hr.employees
+-- select employee_id,first_name,last_name,hire_date,sysdate from hr.employees where row <=2;
+-- select hire_date,add_months(hire_date,6)from hr.employees
+-- select months_between(sysdate,hire_date) from hr.employees
+-- select months_between(sysdate,hire_date) from hr.EMPLOYEES
+-- select months_between(sysdate,hire_date)/12 from hr.EMPLOYEES
+-- select(month_between(sysdate,hire_date))*30 from hr.EMPLOYEES
+
+
+
+
+
+
+
