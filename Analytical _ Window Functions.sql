7854e9caf370c551690776aee1090bccb7d0c110
Poola Naveen <naveenpoola04@gmail.com>
2025-10-13 17:09:44 +0530

    Update Analytical _ Window Functions.sql

--- Analytical _ Window Functions.sql
+++ Analytical _ Window Functions.sql
@@ -1,1 +1,57 @@
 -- Analytical / Window Functions
+
+-- Assign row numbers to customers ordered by credit limit descending.
+-- SELECT 
+    -- customer_id,
+--     customer_name,
+--     credit_limit,
+--     ROWNUM as row_number
+-- FROM (
+--     SELECT 
+--         customer_id,
+--         customer_name,
+--         credit_limit
+     
+--     FROM sh.customers
+--     ORDER BY credit_limit DESC
+-- ); 
+
+
+-- Rank customers within each state by credit limit.
+-- SELECT
+--     cust_first_name,
+--     cust_last_name,
+--     customer_id,
+--     state,                 -- replace with the actual state column if different
+--     credit_limit,          -- replace with actual credit limit column if different
+--     RANK() OVER (
+--         PARTITION BY state
+--         ORDER BY credit_limit DESC
+--     ) AS rank_within_state
+-- FROM
+--     sh.customers;
+-- Use DENSE_RANK() to find the top 5 credit holders per country.k
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
