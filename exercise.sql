5fd8c3902a643ba37f25cf93f1efaee1f83c2847
Poola Naveen <naveenpoola04@gmail.com>
2025-10-24 23:44:08 +0530

    Add files via upload

---
+++ exercise.sql
@@ -0,0 +1,72 @@
+-- select * from hr.EMPLOYEES
+-- select * from hr.EMPLOYEES
+-- select * from hr.EMPLOYEES
+-- select * from hr.employees where salary > 10000
+-- desc hr.EMPLOYEES
+-- select * from hr.employees where employee_id = 101
+-- select * from hr.employees where salary > 10000
+-- select email from hr.EMPLOYEES
+-- select email from hr.EMPLOYEES
+-- select last_name from hr.employees 
+-- select first_name from hr.EMPLOYEES
+-- select first_name || last_name from hr.EMPLOYEES
+-- select * from hr.employees where job_id = 'it_prog'
+-- desc from hr.employees
+-- select hire_date from hr.EMPLOYEES
+-- select department_id from hr.EMPLOYEES
+-- select salary from hr.EMPLOYEES
+-- select manager_id from hr.EMPLOYEES
+-- select phone_number from hr.employees
+-- select commission_pct from hr.EMPLOYEES
+-- select first_name, upper(first_name) from hr.EMPLOYEES
+-- select last_name, upper(last_name) from hr.employees
+-- select first_name, upper(first_name) from hr.EMPLOYEES
+-- select last_name, upper(last_name) from hr.EMPLOYEES
+-- select initcap('poola naveen') from dual
+-- select initcap('edukron for data engineering') from dual
+-- select initcap('naveen belongs to data engineering course')
+-- select initcap('naveen poola') from dual 
+-- select initcap('poola naveen he had joined in edukron inst')
+-- select substr('data science', 2,5) from dual 
+-- select substr('data engineering',2,5) from dual 
+-- select substr('naveen poola', 3,5) from dual
+-- select substr('data science',4,6) from dual
+-- select substr('science', 2,4) from dual 
+-- select instr('data science','science') from DUAL
+-- select instr('data engineering','data') from dual
+-- select instr('machine learning', 'machine') from dual
+-- select instr('edokron data science','edukron data science') from dual
+-- select instr('data science','data') from dual
+-- select instr('data control', 'control') from dual 
+-- select rpad('123','7','0') from DUAL
+-- select lpad('123','5','0') from DUAL
+-- select lpad('9000','9','0') from dual
+-- select lpad('100','7','1') from dual
+-- select rpad('1900','8','0') from dual
+-- select trim('data      ') from dual 
+-- select trim('data engineering') from dual 
+-- select trim('data science') from dual 
+-- select trim('poola      naveen') from dual 
+-- select trim('naveen data engineering') from dual 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
