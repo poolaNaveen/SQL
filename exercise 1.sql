60b518dddb06865e9f8755b95cde02860e946841
Poola Naveen <naveenpoola04@gmail.com>
2025-10-27 20:53:44 +0530

    Add files via upload

---
+++ exercise 1.sql
@@ -0,0 +1,20 @@
+-- select extract(year from hire_date),extract(month from hire_date),extract(day from hire_date) from hr.employees
+-- select hire_date, round(hire_date,'month') from hr.employees
+-- select hire_date,round(hire_date,'month') from hr.EMPLOYEES
+-- select hire_date from hr.EMPLOYEES
+-- select sysdate,last_day(sysdate) from dual 
+-- select sysdate,last_day(sysdate) from dual
+-- select sysdate,hire_date from hr.employees
+-- select sysdate,round(sysdate,'month') from hr.EMPLOYEES
+-- select sysdate,round(sysdate,'day') from hr.EMPLOYEES
+-- select extract(month from hire_date) from hr.EMPLOYEES
+
+
+
+
+
+
+
+
+
+
